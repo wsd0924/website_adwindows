2199c8d0224031b3c77b3ce48aea3f3e882bfdca
wsd0924 <wsd9009@126.com>
2019-09-05 14:45:07 +0800

    website_adwindows_v2.0
    维度传媒车载信息发布系统需求原型v2.0版本

---
+++ files/插播广告详情页（暂无）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="插播广告详情页（暂无）.html",d="generationDate",e=new Date(1567665646979.65),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="num1",l="num2",m="x",n="rulename",o="page",p="packageId",q="56ed6437470f4c409898ed6f09221e3c",r="type",s="Axure:Page",t="name",u="插播广告详情页（暂无）",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0xFFE4E4E4,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
